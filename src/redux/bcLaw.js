159239b1b423db71f2797ec91f3a8a0d46e7c0a8
Victor <victorma07work@gmail.com>
2022-06-15 10:42:33 -0700

    updated with divisions

--- src/redux/bcLaw.js
+++ src/redux/bcLaw.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 export const getLaw = createAsyncThunk('bcLaw/getLaw', async Document_Id => {
-  const url = `https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/${Document_Id}`;
-  //const url = `https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/96126_01`;
+  //const url = `https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/${Document_Id}`;
+  const url = `https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/02057_08#part7`;
   const html = await axios.get(url).then(res => res.data);
   return html;
 });
@@ -12,6 +12,7 @@ export const getLaw = createAsyncThunk('bcLaw/getLaw', async Document_Id => {
 const lawSlice = createSlice({
   name: 'bcLaw',
   initialState: {
+    division: [],
     content: [],
     section: [],
     title: '',
@@ -26,56 +27,96 @@ const lawSlice = createSlice({
       state.status = 'success';
       const contentArray = [];
       const sectionArray = [];
+      const divArray = [];
 
       const html = action.payload;
       const $ = cheerio.load(html);
 
-      const title = {html: $('#title').html()};
+      //const title = {html: $('#title').html()};
+      const title = $('#title').text();
       const part = $('.part').text();
+      const division = $('.division').text();
 
-      $('.section').each(function (i, el) {
-        const content = $(el).find('h4').text();
-        contentArray.push({
-          id: i,
-          title: content,
+      if (!division) {
+        divArray.push({
+          id: 1,
+          divTitle: '',
         });
 
-        const section = $(el).html();
-        sectionArray.push({id: i, html: section});
-      });
+        $('.section').each(function (i, el) {
+          const content = $(el).find('h4').text();
+          contentArray.push({
+            id: i,
+            title: content,
+          });
+
+          const id = $(el).children('p').attr('id');
+
+          const section = $(el).html();
+          sectionArray.push({
+            id: id,
+            divId: '1',
+            divTitle: '',
+            html: section,
+          });
+        });
+      } else {
+        $('.division').each(function (i, el) {
+          const divTitle = $(el).text();
+          const divHtml = $(el).html();
+          const divId = $(el).attr('id');
+
+          contentArray.push({
+            id: i,
+            title: divTitle,
+          });
+
+          divArray.push({
+            id: divId,
+            divTitle: divTitle,
+          });
+
+          sectionArray.push({
+            id: divId,
+            divId: divId,
+            divTitle: divTitle,
+            html: divHtml,
+          });
+
+          $(this)
+            //until next class = division and all class = section between them
+            .nextUntil('.division', '.section')
+            .each(function (i, el) {
+              const content = $(el).find('h4').text();
+
+              contentArray.push({
+                id: i,
+                title: content,
+              });
+
+              const section = $(el).html();
+              //get attr = id from first p tag of children
+              const id = $(el).children('p').attr('id');
+
+              //console.log(id, divID, divTitle);
+              sectionArray.push({
+                id: id,
+                divId: divId,
+                divTitle: divTitle,
+                html: section,
+              });
+            });
+        });
+      }
 
       //console.log(contentArray);
+      //console.log(divArray);
       //console.log(sectionArray);
 
       //for each class = division get id and title
-      /* $('.division').each(function (i, el) {
-        const divTitle = $(el).text();
-        const divId = $(el).attr('id');
-
-        divArray.push({
-          id: divId,
-          divTitle: divTitle,
-        });
-
-        $(this)
-          //until next class = division and all class = section between them
-          .nextUntil('.division', '.section')
-          .each(function (i, el) {
-            const section = $(el).html();
-            //get attr = id from first p tag of children
-            const id = $(el).children('p').attr('id');
-
-            //console.log(id, divID, divTitle);
-            sectionArray.push({
-              id: id,
-              divId: divId,
-              divTitle: divTitle,
-              html: section,
-            });
-          });
-      }); */
 
       //console.log(sectionArray);
+      state.division = divArray;
       state.content = contentArray;
       state.section = sectionArray;
       state.title = title;
