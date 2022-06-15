f4713e04b7cd4dafc82acde495a474087845a8d0
Victor <victorma07work@gmail.com>
2022-06-15 10:47:30 -0700

    Law screen update

--- src/screens/LawScreen.js
+++ src/screens/LawScreen.js
@@ -7,9 +7,16 @@ import {
   FlatList,
   useWindowDimensions,
   Button,
+  SectionList,
 } from 'react-native';
 import RenderHTML from 'react-native-render-html';
-import {IndexPath, Layout, Select, SelectItem} from '@ui-kitten/components';
+import {
+  IndexPath,
+  Layout,
+  Select,
+  SelectGroup,
+  SelectItem,
+} from '@ui-kitten/components';
 
 import {getLaw} from '../redux/bcLaw';
 import SectionComp from '../components/Section';
@@ -17,31 +24,70 @@ import SectionComp from '../components/Section';
 const LawScreen = props => {
   const dispatch = useDispatch();
 
+  const [loading, setLoading] = useState(false);
+
   const content = useSelector(state => state.law.content);
   const title = useSelector(state => state.law.title);
   const part = useSelector(state => state.law.part);
   const section = useSelector(state => state.law.section);
 
+  /* const data = Object.values(
+    section.reduce((group, el) => {
+      group[el.divTitle] = group[el.divTitle] || {
+        title: el.divTitle,
+        data: [],
+      };
+      group[el.divTitle].data.push({
+        id: el.id,
+        divId: el.divId,
+        divTitle: el.divTitle,
+        html: el.html,
+      });
+      return group;
+    }, {}),
+  ); */
+
+  //console.log(data);
+
   const {width} = useWindowDimensions();
   const ref = useRef();
+  const sectionRef = useRef();
 
   const dropDownIndex = 0;
 
   useEffect(() => {
+    setLoading(false);
     dispatch(getLaw(props.route.params.id));
+    setLoading(true);
   }, [dispatch]);
 
   const scrollToIndex = dropDownIndex => {
     ref.current.scrollToIndex({animated: true, index: dropDownIndex});
   };
 
+  const scrollToSection = () => {
+    sectionRef.current.scrollToLocation({
+      animated: true,
+      sectionIndex: 0,
+      itemIndex: 8,
+    });
+  };
+
+  //console.log(section);
+
+  if (!loading) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
   return (
     <View>
       <View style={styles.header}>
-        <RenderHTML contentWidth={width} source={title} />
+        {/* <RenderHTML contentWidth={width} source={title} /> */}
+        <Text>{title}</Text>
         <Text style={styles.part}>{part}</Text>
-      </View>
-      <View>
         <Select
           selectedIndex={dropDownIndex}
           onSelect={index => scrollToIndex(index.row)}
@@ -51,16 +97,41 @@ const LawScreen = props => {
           })}
         </Select>
       </View>
-      <FlatList
-        ref={ref}
-        data={section}
-        initialScrollIndex={dropDownIndex}
-        renderItem={item => (
-          <View>
-            <RenderHTML contentWidth={width} source={item.item} />
-          </View>
-        )}
-      />
+      <View style={styles.container}>
+        <FlatList
+          ref={ref}
+          data={section}
+          initialScrollIndex={dropDownIndex}
+          keyExtractor={item => item.id}
+          onScrollToIndexFailed={info => {
+            const wait = new Promise(resolve => setTimeout(resolve, 500));
+            wait.then(() => {
+              ref.current?.scrollToIndex({index: info.index, animated: true});
+            });
+          }}
+          renderItem={item => (
+            <View>
+              <RenderHTML contentWidth={width} source={item.item} />
+            </View>
+          )}
+        />
+      </View>
+      <View>
+        {/*  <SectionList
+          ref={sectionRef}
+          sections={data}
+          keyExtractor={(item, index) => item + index}
+          initialScrollIndex={dropDownIndex}
+          renderItem={({item}) => (
+            <View>
+              <RenderHTML contentWidth={width} source={item} />
+            </View>
+          )}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+        /> */}
+      </View>
     </View>
   );
 };
@@ -72,7 +143,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   part: {
-    fontSize: 18,
+    fontSize: 12,
     color: 'orange',
     fontWeight: 'bold',
     justifyContent: 'center',
@@ -87,6 +158,9 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     width: 100,
+  },
+  container: {
+    height: '80%',
   },
 });
 
